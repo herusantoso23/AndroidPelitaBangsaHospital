969824c88b0248039a730b50c75c58708cbaa53b
HeruSantoso <herusantoso008@gmail.com>
2017-08-24 19:58:01 +0700

    Update

--- app/src/main/java/com/tugas/pelitabangsa/herusantoso/pelitabangsahospital/common/AppDefinition.java
+++ app/src/main/java/com/tugas/pelitabangsa/herusantoso/pelitabangsahospital/common/AppDefinition.java
@@ -5,7 +5,7 @@ package com.tugas.pelitabangsa.herusantoso.pelitabangsahospital.common;
  */
 
 public class AppDefinition {
-    private static String link = "http://192.168.1.23/belajar_api/pb_hospital/";
+    private static String link = "http://192.168.43.173/belajar_api/pb_hospital/";
 
     public static final String URL = link;
 
